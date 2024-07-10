ad0f1b0d5ebda0216d8ab2a913d4860ac609c7a8
Charles Renan Retes <retescharlesrenan@gmail.com>
2024-07-10 08:30:17 +0800

    S - Tailwind colors are extended

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,20 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "bg-color": "#323437",
+        "main-color": "#e2b714",
+        "caret-color": "#e2b714",
+        "sub-color": "#646669",
+        "sub-alt-color": "#2c2e31",
+        "text-color": "#d1d0c5",
+        "error-color": "#ca4754",
+        "error-extra-color": "#7e2a33",
+        "colorful-error-color": "#ca4754",
+        "colorful-error-extra-color": "#7e2a33",
       },
     },
   },
