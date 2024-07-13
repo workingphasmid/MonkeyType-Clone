1a7ca7dd7b7e8e42ef7ad3952bbaec8414c698c2
Charles Renan Retes <retescharlesrenan@gmail.com>
2024-07-13 20:38:02 +0800

    S -  Header Icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,30 @@
 import React from "react";
+import Image from "next/image";
+import logoIcon from "public/logo-icon.svg";
+import { FaKeyboard, FaCrown, FaInfo, FaCog, FaBell, FaRegUser } from "react-icons/fa";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      {/* Left Section */}
+      <div>
+        <div>
+          <Image src={logoIcon} alt="Logo Icon" width={40} />
+          <span>monkeytype</span>
+        </div>
+        <div>
+          <FaKeyboard size={20} />
+          <FaCrown size={20} />
+          <FaInfo size={20} />
+          <FaCog size={20} />
+        </div>
+      </div>
+
+      {/* Right Section */}
+      <div>
+        <FaBell />
+        <FaRegUser />
+      </div>
+    </div>
+  );
 }
