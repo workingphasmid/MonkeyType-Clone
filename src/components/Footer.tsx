c51db41f89c9febe9714f88195e0bb520c37dc70
Charles Renan Retes <retescharlesrenan@gmail.com>
2024-07-18 00:46:28 +0800

    D - Footer icons

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import {
+  FaEnvelope,
+  FaDonate,
+  FaCode,
+  FaDiscord,
+  FaTwitter,
+  FaFileContract,
+  FaShieldAlt,
+  FaLock,
+} from "react-icons/fa";
+
+export default function Footer() {
+  return <div>Footer</div>;
+}
