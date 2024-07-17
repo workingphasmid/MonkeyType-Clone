cb6b2f356b5e96f5a043b20bc3bf912bc04ff850
Charles Renan Retes <retescharlesrenan@gmail.com>
2024-07-18 01:09:12 +0800

    D - Footer without effects

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Roboto_Mono, Lexend_Deca } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const robotomono = Roboto_Mono({ subsets: ["latin"] });
 export const lexenddeca = Lexend_Deca({ subsets: ["latin"] });
@@ -19,10 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${robotomono.className} flex h-screen flex-col bg-bg-color px-16 py-8`}
+        className={`${robotomono.className} flex h-screen flex-col bg-bg-color px-16 pb-6 pt-8`}
       >
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import { IconType } from "react-icons";
 import {
   FaEnvelope,
   FaDonate,
@@ -7,8 +8,36 @@ import {
   FaFileContract,
   FaShieldAlt,
   FaLock,
+  FaPalette,
+  FaCodeBranch,
 } from "react-icons/fa";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="flex items-center justify-between pt-6">
+      <div className="flex gap-4">
+        <Items Icon={FaEnvelope} title="contact" />
+        <Items Icon={FaDonate} title="support" />
+        <Items Icon={FaCode} title="github" />
+        <Items Icon={FaDiscord} title="discord" />
+        <Items Icon={FaTwitter} title="twitter" />
+        <Items Icon={FaFileContract} title="terms" />
+        <Items Icon={FaShieldAlt} title="security" />
+        <Items Icon={FaLock} title="privacy" />
+      </div>
+      <div className="flex gap-4">
+        <Items Icon={FaPalette} title="serika dark" />
+        <Items Icon={FaCodeBranch} title="v.24.29.0" />
+      </div>
+    </div>
+  );
+}
+
+function Items({ title, Icon }: { title: string; Icon: IconType }) {
+  return (
+    <div className="flex items-center gap-2 text-[12px] text-sub-color">
+      <Icon />
+      {title}
+    </div>
+  );
 }
