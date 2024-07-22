c704e25cdc7f39e5cdd5eca45bd67c6a3ad84f5d
Charles Renan Retes <retescharlesrenan@gmail.com>
2024-07-22 19:47:56 +0800

    L - wordRef working

--- src/components/ToType.tsx
+++ src/components/ToType.tsx
@@ -1,47 +1,70 @@
 "use client";
 
 import { words } from "@/lib/data";
-import { useRef, useState } from "react";
+import { forwardRef, useRef, useState } from "react";
 
-type WordType = { word: string };
+type WordType = {
+  word: string;
+  wordIndex: number;
+  currentWordIndex: number;
+  setCurrentWordIndex: any;
+  ref: React.RefObject<HTMLDivElement>;
+};
 
 export default function ToType() {
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+
+  const currentWordRef = useRef<HTMLDivElement>(null);
+
+  console.log(currentWordRef.current);
   return (
     <div className="relative px-16 text-2xl">
       <Caret />
       {words.map((word, i) => (
-        <Word word={word} key={i} />
+        <Word
+          key={i}
+          word={word}
+          wordIndex={i}
+          currentWordIndex={currentWordIndex}
+          setCurrentWordIndex={setCurrentWordIndex}
+          ref={currentWordRef}
+        />
       ))}
     </div>
   );
 }
 
-function Word({ word }: WordType) {
-  const [letterIndex, setLetterIndex] = useState(0);
-  const currentLetterRef = useRef<HTMLSpanElement>(null);
+const Word = forwardRef<HTMLDivElement, WordType>(
+  ({ word, wordIndex, currentWordIndex, setCurrentWordIndex }, ref) => {
+    const [letterIndex, setLetterIndex] = useState(0);
 
-  const letters = word.split("");
+    const letters = word.split("");
 
-  function handleKeydown(e: any) {
-    setLetterIndex(letterIndex + 1);
-  }
+    function handleKeydown(e: any) {
+      if (word.length === letterIndex + 1) {
+        setCurrentWordIndex(currentWordIndex + 1);
+        setLetterIndex(0);
+      } else {
+        setLetterIndex(letterIndex + 1);
+      }
+    }
 
-  return (
-    <div
-      className="mx-2 inline-block"
-      tabIndex={0}
-      autoFocus={true}
-      onKeyDown={handleKeydown}
-    >
-      {letters.map((letter, i) => (
-        <span key={i} ref={letterIndex === i ? currentLetterRef : null}>
-          {letter}
-        </span>
-      ))}
-      {letterIndex}
-    </div>
-  );
-}
+    return (
+      <div
+        className="mx-2 inline-block"
+        tabIndex={0}
+        autoFocus={true}
+        onKeyDown={handleKeydown}
+        ref={currentWordIndex === wordIndex ? ref : null}
+      >
+        {letters.map((letter, i) => (
+          <span key={i}>{letter}</span>
+        ))}
+        {letterIndex}
+      </div>
+    );
+  },
+);
 
 function Caret() {
   return (
