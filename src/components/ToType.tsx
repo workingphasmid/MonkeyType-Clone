0467cbc8c01caf241e1e38e1aaf2789d025ae99b
Charles Renan Retes <retescharlesrenan@gmail.com>
2024-07-23 16:03:17 +0800

    L - WordIndex in Word component path - not working

--- src/components/ToType.tsx
+++ src/components/ToType.tsx
@@ -1,148 +1,147 @@
 "use client";
 
 import { words } from "@/lib/data";
-import { forwardRef, useRef, useState, KeyboardEvent } from "react";
+import { useRef, useState, KeyboardEvent } from "react";
 
 type WordType = {
   word: string;
   wordIndex: number;
-  currentWordIndex: number;
-  updateCurrentWord: any;
-  ref: React.RefObject<HTMLDivElement>;
 };
 
 export default function ToType() {
-  const [currentWordIndex, setCurrentWordIndex] = useState(1);
-  const currentWordRef = useRef<HTMLDivElement>(null);
-
-  function updateCurrentWord(e: KeyboardEvent) {
-    setCurrentWordIndex(currentWordIndex + 1);
-    currentWordRef.current?.focus();
-  }
-
   return (
     <div className="relative px-16 text-2xl">
       <Caret />
       {words.map((word, i) => (
-        <Word
-          key={i}
-          word={word}
-          wordIndex={i}
-          currentWordIndex={currentWordIndex}
-          updateCurrentWord={updateCurrentWord}
-          ref={currentWordRef}
-        />
+        <Word key={i} word={word} wordIndex={i} />
       ))}
     </div>
   );
 }
 
-const Word = forwardRef<HTMLDivElement, WordType>(
-  ({ word, wordIndex, currentWordIndex, updateCurrentWord }, ref) => {
-    const [letterIndex, setLetterIndex] = useState(0);
-    const [letters, setLetters] = useState(word.split(""));
-    const [lettersColor, setLettersColor] = useState(Array<string>);
+function Word({ word, wordIndex }: WordType) {
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
 
-    function handleKeydown(e: KeyboardEvent) {
-      const atEnd = letters.length === letterIndex;
-      const pressedKey = e.key;
-      const pattern = /^[\s\w'",.?!;:@#$%&()]$/;
+  const [letterIndex, setLetterIndex] = useState(0);
+  const [letters, setLetters] = useState(word.split(""));
+  const [lettersColor, setLettersColor] = useState(Array<string>);
 
-      // Wall: Ending the key if its not valid
-      if (!pattern.test(pressedKey) && pressedKey !== "Backspace") {
-        return;
-      }
+  const wordRefs = useRef<(HTMLDivElement | null)[]>([]);
 
-      // Updating the word and letter indexes
-      const isFirstLetter = letterIndex === 0;
-      const isSpace = pressedKey === " ";
+  function handleKeydown(e: KeyboardEvent) {
+    const atEnd = letters.length === letterIndex;
+    const pressedKey = e.key;
+    const pattern = /^[\s\w'",.?!;:@#$%&()-+]$/;
 
-      if (pressedKey === "Backspace") {
-        deleteLetter(pressedKey);
-        // updateLettersColor("default");
-      } else if (isSpace && !isFirstLetter) {
-        updateCurrentWord(e);
-      } else if (!atEnd && !isSpace) {
-        setLetterIndex(letterIndex + 1);
-        checkLetter(pressedKey);
-      } else if (!isSpace) {
-        updateLetters(pressedKey, "add");
-        updateLettersColor("wrong");
-      }
+    // Wall: Ending the key if its not valid
+    if (!pattern.test(pressedKey) && pressedKey !== "Backspace") {
+      return;
     }
 
-    // Supporting functions
-    function deleteLetter(pressedKey: string) {
-      if (word.length < letterIndex) {
-        updateLetters(pressedKey, "delete");
-      } else if (letterIndex > 0) {
-        setLetterIndex(letterIndex - 1);
-      }
+    // Updating the word and letter indexes
+    const isFirstLetter = letterIndex === 0;
+    const isSpace = pressedKey === " ";
 
-      const newLettersColor = [...lettersColor];
-      newLettersColor.pop();
+    if (pressedKey === "Backspace") {
+      deleteLetter(pressedKey);
+    } else if (isSpace && !isFirstLetter) {
+      updateCurrentWord("add");
+    } else if (!atEnd && !isSpace) {
+      setLetterIndex(letterIndex + 1);
+      checkLetter(pressedKey);
+    } else if (!isSpace) {
+      updateLetters(pressedKey, "add");
+      updateLettersColor("wrong");
+    }
+  }
 
-      setLettersColor(newLettersColor);
+  // Supporting functions
+  function deleteLetter(pressedKey: string) {
+    if (word.length < letterIndex) {
+      updateLetters(pressedKey, "delete");
+    } else if (letterIndex > 0) {
+      setLetterIndex(letterIndex - 1);
+    } else if (letterIndex === 0 && wordIndex !== 0) {
+      updateCurrentWord("minus");
+      setLetterIndex(letterIndex - 1);
     }
 
-    function updateLetters(pressedKey: string, mode: string) {
-      const newLetters = [...letters];
+    const newLettersColor = [...lettersColor];
+    newLettersColor.pop();
 
-      if (mode === "delete") {
-        newLetters.pop();
+    setLettersColor(newLettersColor);
+  }
 
-        setLetterIndex(letterIndex - 1);
-      } else if (mode === "add") {
-        newLetters[letterIndex] = pressedKey;
+  function updateLetters(pressedKey: string, mode: string) {
+    const newLetters = [...letters];
 
-        setLetterIndex(letterIndex + 1);
-      }
+    if (mode === "delete") {
+      newLetters.pop();
 
-      setLetters(newLetters);
+      setLetterIndex(letterIndex - 1);
+    } else if (mode === "add") {
+      newLetters[letterIndex] = pressedKey;
+
+      setLetterIndex(letterIndex + 1);
     }
 
-    function updateLettersColor(mode: string) {
-      const newLettersColor = [...lettersColor];
+    setLetters(newLetters);
+  }
 
-      if (mode === "correct") {
-        newLettersColor[letterIndex] = "text-text-color";
-      } else if (mode === "wrong") {
-        newLettersColor[letterIndex] = "text-error-color";
-      } else if (mode === "default") {
-        newLettersColor[letterIndex] = "text-sub-color";
-      }
+  function updateLettersColor(mode: string) {
+    const newLettersColor = [...lettersColor];
 
-      setLettersColor(newLettersColor);
+    if (mode === "correct") {
+      newLettersColor[letterIndex] = "text-text-color";
+    } else if (mode === "wrong") {
+      newLettersColor[letterIndex] = "text-error-color";
+    } else if (mode === "default") {
+      newLettersColor[letterIndex] = "text-sub-color";
     }
 
-    function checkLetter(pressedKey: string) {
-      const currentLetter = letters[letterIndex];
+    setLettersColor(newLettersColor);
+  }
 
-      if (currentLetter === pressedKey) {
-        updateLettersColor("correct");
-      } else {
-        updateLettersColor("wrong");
-      }
+  function checkLetter(pressedKey: string) {
+    const currentLetter = letters[letterIndex];
+
+    if (currentLetter === pressedKey) {
+      updateLettersColor("correct");
+    } else {
+      updateLettersColor("wrong");
+    }
+  }
+
+  function updateCurrentWord(mode: string) {
+    if (mode === "add") {
+      setCurrentWordIndex(currentWordIndex + 1);
+    } else if (mode === "minus") {
+      setCurrentWordIndex(currentWordIndex - 1);
     }
 
-    const isAutoFocus = wordIndex === 0;
-    return (
-      <div
-        className="mx-2 inline-block outline-none"
-        onKeyDown={(e) => handleKeydown(e)}
-        ref={currentWordIndex === wordIndex ? ref : null}
-        tabIndex={0}
-        autoFocus={isAutoFocus}
-      >
-        {letters.map((letter, i) => (
-          <span key={i} className={lettersColor[i]}>
-            {letter}
-          </span>
-        ))}
-      </div>
-    );
-  },
-);
+    console.log(wordRefs.current);
+    wordRefs.current[currentWordIndex]?.focus();
+  }
+
+  const isAutoFocus = wordIndex === 0;
+  return (
+    <div
+      className="mx-2 inline-block"
+      onKeyDown={(e) => handleKeydown(e)}
+      ref={(el) => {
+        wordRefs.current[wordIndex] = el;
+      }}
+      tabIndex={0}
+      autoFocus={isAutoFocus}
+    >
+      {letters.map((letter, i) => (
+        <span key={i} className={lettersColor[i]}>
+          {letter}
+        </span>
+      ))}
+    </div>
+  );
+}
 
 function Caret() {
   return (
